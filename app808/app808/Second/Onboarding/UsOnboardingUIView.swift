cda9f9dd3392e8ce4efa297dc6a6a50924d86a48
Dias <dias.atudinov@gmail.com>
2024-08-09 19:11:13 +0500

    Content add

---
+++ app808/app808/Second/Onboarding/UsOnboardingUIView.swift
@@ -0,0 +1,18 @@
+//
+//  UsOnboardingUIView.swift
+//  app808
+//
+//  Created by Dias Atudinov on 09.08.2024.
+//
+
+import SwiftUI
+
+struct UsOnboardingUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UsOnboardingUIView()
+}
---
+++ app808/app808/app808App.swift
@@ -0,0 +1,17 @@
+//
+//  app808App.swift
+//  app808
+//
+//  Created by Dias Atudinov on 09.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct app808App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
