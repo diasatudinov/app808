71e7aa2e30ffd2bd77dc5c0c1a768cd8c2e18d23
Dias <dias.atudinov@gmail.com>
2024-08-27 14:25:09 +0500

    new files

---
+++ app808/app808/First/Diary/View/NotesCellUIView.swift
@@ -0,0 +1,18 @@
+//
+//  NotesCellUIView.swift
+//  app808
+//
+//  Created by Dias Atudinov on 27.08.2024.
+//
+
+import SwiftUI
+
+struct NotesCellUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotesCellUIView()
+}
