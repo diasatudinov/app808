955371c838b786f283d970aa65507d2396c821d3
Dias <dias.atudinov@gmail.com>
2024-08-28 15:20:51 +0500

    Preporation for update: AppDelegate

--- app808/app808/AppDelegate.swift
+++ app808/app808/AppDelegate.swift
@@ -5,4 +5,33 @@
 //  Created by Dias Atudinov on 28.08.2024.
 //
 
-import Foundation
+
+import UIKit
+import ApphudSDK
+import YandexMobileMetrica
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        Apphud.start(apiKey: Info().appHudID)
+
+        let configuration = YMMYandexMetricaConfiguration.init(apiKey: Info().metricaID)
+        YMMYandexMetrica.activate(with: configuration!)
+        
+        print("AppDelegate: didFinishLaunchingWithOptions")
+        return true
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Handle app entering background
+        print("AppDelegate: applicationDidEnterBackground")
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Handle app entering foreground
+        print("AppDelegate: applicationWillEnterForeground")
+    }
+    
+    // Implement other UIApplicationDelegate methods as needed
+}
--- app808/app808/app808App.swift
+++ app808/app808/app808App.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 @main
 struct app808App: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
            LoaderUIView()
