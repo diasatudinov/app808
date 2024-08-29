59cee2213ab3c1e87d8cb2c1c828b33bcaa350d1
Dias <dias.atudinov@gmail.com>
2024-08-29 14:44:21 +0500

    fix

--- app808/app808/Manager/Info.swift
+++ app808/app808/Manager/Info.swift
@@ -13,7 +13,7 @@ struct Info {
     let appHudID = "app_c4KyQodikFCEN14CiXpoKvGaAZUFq5"
     let metricaID = "9f28cb4b-3479-4780-a789-02a1b6b04fdf"
     let usagePolicy = "https://www.termsfeed.com/live/d8c1e4cc-40b1-4cb4-b602-9b2f4b3a23b1"
-    let oneSignal = "16ff35e-1ff0-426d-a3ed-faac5e5240d6"
+    let oneSignal = "a16ff35e-1ff0-426d-a3ed-faac5e5240d6"
     let techCode: String = "scheduled"
     
 }
